547e849142be93572f3a4bdd4a6cb55a48dc571a
June Choi <fanse2@gmail.com>
2022-06-21 11:11:54 -0700

    1st

---
+++ myplotlib.js
@@ -0,0 +1,30 @@
+class XYPlotter {
+    constructor(id) {
+        this.canvas = document.querySelector('#'+id)
+        this.ctx = this.canvas.getContext('2d')
+        this.xMin = 0
+        this.yMin = 0
+        this.xMax = this.canvas.Width
+        this.yMax = this.canvas.height
+    }
+
+    plotline(x0,y0,x,y,color) {
+        this.ctx.moveTo(x0,y0)
+        this.ctx.lineTo(x,y)
+        this.ctx.strokeStyle = color
+        this.ctx.stroke()
+    }
+
+    transformXY() {
+        this.ctx.transform(1,0,0,-1,0,this.canvas.height)
+    }
+
+    plotPoint(n,xArr,yArr,color,radius = 0) {
+        for(let i=0; i<n; i++) {
+            this.ctx.fillStyle = color
+            this.ctx.beginPath()
+            this.ctx.ellipse(xArr[i],yArr[i],radius,radius,0,0,Math.PI*2)
+            this.ctx.fill()
+        }
+    }
+}
